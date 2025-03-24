fa1f0bd53e7a3116ca473253453daa7295c4d094
Akshita Srivastava <akshita.srivastava.1505@gmail.com>
2025-03-24 22:28:51 +0530

    it

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,13 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
 const isAdminRoute = createRouteMatcher(['/admin(.*)'])
-const isPublicRoute = createRouteMatcher(['/api/webhooks/clerk(.*)'])
+
 export default clerkMiddleware(async (auth, req) => {
   // Protect all routes starting with `/admin`
   if (isAdminRoute(req) && (await auth()).sessionClaims?.metadata?.role !== 'hospital_admin') {
     const url = new URL('/', req.url)
     return NextResponse.redirect(url)
   }
-  if (!isPublicRoute(req)) {
-    await auth.protect()
-  }
-
 })
 
 export const config = {
