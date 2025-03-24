a8637672ad57fa3e6d228a9a41f050c7c1c14db2
Akshita Srivastava <akshita.srivastava.1505@gmail.com>
2025-03-24 20:57:14 +0530

    webhooksetup

--- src/lib/models/user.model.ts
+++ src/lib/models/user.model.ts
@@ -15,10 +15,7 @@ const UserSchema=new Schema({
         type:String,
         required:true
     },
-    password:{
-        type:String,
-        required:true
-    },
+    
     role:{
         type:String,
         default:"user"
